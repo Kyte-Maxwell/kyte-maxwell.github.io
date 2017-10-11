92e08741f07bd375389d47d6c1eed712f18ded23
kyte-maxwell <kyt09001@byui.edu>
2017-10-11 14:09:02 -0700

    Add files via upload

--- AJAXTest.js
+++ AJAXTest.js
@@ -1,12 +1,10 @@
 function getJSON() {
   var variable = new XMLHttpRequest();
-  var jsonText;
   variable.onreadystatechange = function() {
-	   if (variable.readyState == 4 && variable.status == 200) {
-		     jsonText = JSON.parse(variable.responseText);
+	   if (this.readyState == 4 && this.status == 200) {
+		     document.getElementById("Output").innerHTML = this.responseText;
 	      }
       };
-      variable.open("GET", "https://kyte-maxwell.github.io/JSONTest.txt", true);
+      variable.open("GET", "JSONTest.txt", true);
       variable.send();
-      document.getElementById("Output").innerHTML = jsonText;
 }
